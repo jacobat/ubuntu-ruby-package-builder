65b033c98837229302d1b81d8863c0cb0d81c1e7
Jacob Atzen <jacob@incremental.dk>
2012-04-09 13:10:18 +0200

    New builds

---
+++ build-deb-ruby-1.9.2-p290.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+rubyversion=1.9.2-p290
+rubysrc=ruby-$rubyversion.tar.bz2
+checksum=096758c3e853b839dc980b183227b182
+destdir=/tmp/install-$rubyversion
+
+sudo apt-get install libssl-dev
+
+gem list -i fpm || sudo gem install fpm
+
+if [ ! -f $rubysrc ]; then
+  wget -q ftp://ftp.ruby-lang.org/pub/ruby/1.9/$rubysrc
+fi
+
+if [ "$(md5sum $rubysrc | cut -b1-32)" != "$checksum" ]; then
+  echo "Checksum mismatch!"
+  exit 1
+fi
+
+echo "Unpacking $rubysrc"
+tar -jxf $rubysrc
+cd ruby-$rubyversion
+./configure --prefix=/usr && make && make install DESTDIR=$destdir
+
+cd ..
+fpm -s dir -t deb -n ruby-$rubyversion -v $rubyversion -C $destdir \
+  -p ruby-VERSION_ARCH.deb -d "libstdc++6 (>= 4.4.3)" \
+  -d "libc6 (>= 2.6)" -d "libffi5 (>= 3.0.4)" -d "libgdbm3 (>= 1.8.3)" \
+  -d "libncurses5 (>= 5.7)" -d "libreadline6 (>= 6.1)" \
+  -d "libssl0.9.8 (>= 0.9.8)" -d "zlib1g (>= 1:1.2.2)" \
+  usr/bin usr/lib usr/share/man usr/include
+
+rm -r $destdir
